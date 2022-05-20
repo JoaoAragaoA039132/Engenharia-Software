8a6734bbf43893f509598a36738e977c4ebdfffb
a038472 <82144934+a038472@users.noreply.github.com>
2022-05-20 14:37:17 +0100

    Update 2022_05_20_133442_categorias_table.php

--- restaurante/database/migrations/2022_05_20_133442_categorias_table.php
+++ restaurante/database/migrations/2022_05_20_133442_categorias_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id_categoria()->unique();
+            $table->string('categoria');
+            $table->string('nome');
+        });
     }
 
     /**
