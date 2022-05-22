f8c365689f6effbdd7d41efdb010b198732c101c
BernardoMagalhaes13 <82144870+BernardoMagalhaes13@users.noreply.github.com>
2022-05-22 23:22:54 +0100

    Entrega Final

---
+++ EntregaFinal/Analise/BD/migrations/2022_05_19_175805_menu_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->integer('categoria');
+            $table->string('preco');
+            $table->string('nome');
+            $table->string('descricao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
