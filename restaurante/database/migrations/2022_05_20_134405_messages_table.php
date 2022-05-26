2d927a89162a3428d8dfccec92ce4e1a4ed668d5
JoaoAragaoA039132 <a039132@ismai.pt>
2022-05-26 13:41:08 +0100

    Update 2022_05_20_134405_messages_table.php

--- restaurante/database/migrations/2022_05_20_134405_messages_table.php
+++ restaurante/database/migrations/2022_05_20_134405_messages_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id()->unique();
-            $table->int('cliente');
+            $table->integer('cliente');
             $table->string('mensagem');
             $table->timestamp('data_mensagem');
         });
