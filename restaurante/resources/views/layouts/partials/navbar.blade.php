aca04b346eacbf83db6ecd4aa0f79fb4b928c454
JoaoAragaoA039132 <a039132@ismai.pt>
2022-05-31 11:55:48 +0100

    .

---
+++ restaurante/resources/views/layouts/partials/navbar.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">ISMAI</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+
+        </ul>
+        @auth
+            {{auth()->user()->name}}
+            <div class="text-end">
+                <a href="{{route('logout.perform')}}" class="btn btn-outline-secondary">Logout</a>
+            </div>
+        @endauth
+        @guest
+            <div class="text-end">
+                <a href="{{route('login.perform')}}" class="btn btn-outline-secondary">Login</a>
+                <a href="{{route('register.perform')}}" class="btn btn-outline-secondary">Registar</a>
+            </div>
+        @endguest
+      </div>
+    </div>
+  </nav>
