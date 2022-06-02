8e188a2533b6a4e711a07aeaa19986fb33c9b414
JoaoAragaoA039132 <a039132@ismai.pt>
2022-06-02 16:57:21 +0100

    Update 2022_05_19_180037_reservas_table.php

--- restaurante/database/migrations/2022_05_19_180037_reservas_table.php
+++ restaurante/database/migrations/2022_05_19_180037_reservas_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->integer('n_pessoas');
             $table->datetime('data_reserva');
             $table->integer('estado');
+            $table->timestamps();
         });
     }
 
