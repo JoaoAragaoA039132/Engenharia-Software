4784129387571525f37e752620c351192cda9d24
JoaoAragaoA039132 <a039132@ismai.pt>
2022-06-09 16:13:08 +0100

    .

--- restaurante/app/Http/Controllers/MessagesController.php
+++ restaurante/app/Http/Controllers/MessagesController.php
@@ -19,11 +19,7 @@ class MessagesController extends Controller
         return redirect()->route('message.show')->with('Mensagem envida','Aguarde pela confirmação');
     }
 
-    function editf(Reservations $reservations){
-        return view('messages-func.edit',['messages'=>$messages]);
-    }
-
-    function updatef(){
-
+    function showf(Messages $messages){
+        return view('messages-func.show');
     }
 }
--- restaurante/resources/views/messages-func/show.blade.php
+++ restaurante/resources/views/messages-func/show.blade.php
@@ -1,1 +1,36 @@
-<h1>TESTE</h1>
+@extends('layouts.app-master-no-navbar')
+@section('content')
+<form method="POST" {{route('messages.createc')}}>
+    @csrf
+    <div class="bg-light p-5 rounded" style="margin:10px;">
+
+        @guest
+            <h1>Mensagens</h1>
+        @endguest
+
+        <br>
+
+        <div style="text-align: center; align-right;" >
+            <table>
+                <tr>
+                    <th class="col-4" style="border: 2px solid #000000"> Nome</th>
+                    <th class="col-8" style="border: 2px solid #000000">Mensagem</th>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #000000">2</td>
+                    <td style="border: 1px solid #000000">Zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #000000">1</td>
+                    <td style="border: 1px solid #000000">Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</td>
+                </tr>
+            </table>
+            <br>
+            <div>
+                <input type="submit" class="btn btn-primary" name="Voltar" value="Voltar">
+            </div>
+        </div>
+</form>
+
+
+@endsection
