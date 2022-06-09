dad6e06c8797c69addc2d51065e7b676f7b2d8c1
JoaoAragaoA039132 <a039132@ismai.pt>
2022-06-09 16:27:43 +0100

    Update show.blade.php

--- restaurante/resources/views/messages-func/show.blade.php
+++ restaurante/resources/views/messages-func/show.blade.php
@@ -2,35 +2,27 @@
 @section('content')
 <form method="POST" {{route('messages.createc')}}>
     @csrf
-    <div class="bg-light p-5 rounded" style="margin:10px;">
-
-        @guest
-            <h1>Mensagens</h1>
-        @endguest
-
-        <br>
-
-        <div style="text-align: center; align-right;" >
-            <table>
-                <tr>
-                    <th class="col-4" style="border: 2px solid #000000"> Nome</th>
-                    <th class="col-8" style="border: 2px solid #000000">Mensagem</th>
-                </tr>
-                <tr>
-                    <td style="border: 1px solid #000000">2</td>
-                    <td style="border: 1px solid #000000">Zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz</td>
-                </tr>
-                <tr>
-                    <td style="border: 1px solid #000000">1</td>
-                    <td style="border: 1px solid #000000">Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</td>
-                </tr>
-            </table>
-            <br>
-            <div>
-                <input type="submit" class="btn btn-primary" name="Voltar" value="Voltar">
-            </div>
-        </div>
-</form>
-
-
+    <br>
+<h1 class="h3 mb-3 fw-normal">Lista de reservas</h1>
+<div style="text-align: center; align-right;" >
+    <table>
+        <tr>
+            <th class="col-2" style="border: 2px solid #000000"> Nome</th>
+            <th class="col-11" style="border: 2px solid #000000">Nº Pessoas</th>
+        </tr>
+        <tr style=>
+            <td style="border: 1px solid #000000">Exemplo 1</td>
+            <td style="border: 1px solid #000000">2</td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid #000000">Exemplo 2</td>
+            <td style="border: 1px solid #000000">4</td>
+            <td><a href="/reservations-func/edit" class="btn btn-light" >Alterar</a></td>
+        </tr>
+    </table>
+    <br>
+    <div>
+        <a href="/puser" class="btn btn-light" >Voltar</a>
+    </div>
+</div>
 @endsection
