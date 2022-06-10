23e3267c917c6c2d0e02e2d634880eda52bd224d
JoaoAragaoA039132 <a039132@ismai.pt>
2022-06-10 22:29:56 +0100

    Update show.blade.php

---
+++ restaurante/resources/views/menu/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app-master')
+@section('content')
+    <div class="bg-light p-5 rounded" style="margin:10px;">
+
+        <form method="POST" {{route('menu.show')}}>
+            @csrf
+            <br>
+            <h1 class="h3 mb-3 fw-normal">O Nosso Menu</h1>
+            <form style="text-align: center; align-right;" >
+            <table class="table table-striped">
+                <tr>
+                    <th class="col-2" style="border: 2px solid #000000">Nome: </th>
+                    <th class="col-10" style="border: 2px solid #000000">Mensagem: </th>
+                </tr>
+
+                @foreach($messages as $mes)
+                    <tr style=>
+                        <td style="border: 1px solid #000000">{{$mes->nome}}</td>
+                        <td style="border: 1px solid #000000">{{$mes->mensagem}}</td>
+                    </tr>
+                @endforeach
+            </table>
+            <br>
+            <div>
+                <input type="button" class="btn btn-primary" value="Voltar" onClick="history.go(-1)">
+            </div>
+        </form>
+    </div>
+@endsection
