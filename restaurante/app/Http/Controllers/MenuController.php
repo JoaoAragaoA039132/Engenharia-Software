15bca9e9dfebbfb9b99865ac1fea96e97448a0c4
JoaoAragaoA039132 <a039132@ismai.pt>
2022-06-14 17:09:01 +0100

    .

--- restaurante/app/Http/Controllers/MenuController.php
+++ restaurante/app/Http/Controllers/MenuController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Menu;
 
 class MenuController extends Controller
 {
     function show(Menus $menus){
         $menus=Menus::latest()->paginate(15);
         //return $messages;
-        return view('menu.show', ['menu' => $menu]);
+        //return view('menu.show', ['menu' => $menu]);
+        return true;
     }
 }
